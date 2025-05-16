0439f5009d445c9fed582891ace31b3db88443e2
Awais Zahid <mailforawais@.com>
2025-05-17 02:11:42 +0500

    login fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -59,6 +59,8 @@ export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
   },
   cookies: {
     sessionToken: {
@@ -89,6 +91,8 @@ export const authOptions = {
       return session;
     },
   },
+
+  useSecureCookies: process.env.NEXTAUTH_URL.startsWith("https://"),
 };
 
 const handler = NextAuth(authOptions);

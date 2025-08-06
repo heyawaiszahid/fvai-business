7295801459a8eefc891aeceabe243b859afb195c
Awais Zahid <mailforawais@.com>
2025-08-06 17:47:22 +0500

    misc 6

--- app/(private)/dashboard/Chat.jsx
+++ app/(private)/dashboard/Chat.jsx
@@ -9,7 +9,7 @@ export default function Chat() {
     <div className="h-full flex flex-col">
       <div className="flex-1 overflow-y-auto py-4 lg:px-4"></div>
 
-      <div className="bg-white px-6 py-2 flex items-center gap-6 -ml-5 lg:ml-0 -mr-5 lg:mr-0">
+      <div className="bg-white px-6 py-2 flex items-center gap-4 lg:gap-6">
         <div className="flex-1">
           <input
             type="text"
@@ -17,7 +17,7 @@ export default function Chat() {
             className="w-full lg:px-4 py-4 placeholder:text-pale-blue text-[15px] outline-0"
           />
         </div>
-        <div className="shrink-0 flex gap-6">
+        <div className="shrink-0 flex gap-4 lg:gap-6">
           <Button variant="light" className="py-2 px-2">
             <Attach />
           </Button>
